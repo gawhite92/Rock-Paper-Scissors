553cc0924f1c63042492a22f11215934674a5993
Graeme <122578880+gawhite92@users.noreply.github.com>
2024-06-21 10:15:53 +0800

    HTML boilerplate & javascript set up. Rough pseudocode in .js

---
+++ javascript.js
@@ -0,0 +1,20 @@
+//Pseudocode
+//- Ask input for playerHand, assign to variable -> function getHumanChoice (uses prompt)
+//- Calculate randomised input for computerHand -> function getComputerChoice (uses Math.random)
+//2 , 3 - "Lose! Scissors beats paper!"
+//- Assign the above computerHand return to variable
+//- Assign shorthand variables for rock / paper / scissors. r / p / s, or 1 = Rock, 2 = Paper, 3 = Scissors.
+//- Calculate win / lose / draw, code all scenarios. Below table.
+//- Display outcome to user. Below table.
+//
+//1 - 1 - "Draw!"
+//1 , 2 - "Lose! Paper beats rock!"
+//1 , 3 - "Win! Rock beats paper!"
+//
+//2 , 1 - "Win! Paper beats rock!"
+//2 , 2 - "Draw!"
+//3 , 3 - "Draw!"
+//
+//3 , 1 - "Lose! Rock beats scissors!"
+//3 , 2 - "Win! Scissors beats paper!"
+//- Update score - need to research this
